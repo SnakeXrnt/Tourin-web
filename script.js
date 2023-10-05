56af085bd01830c5d5f1085c2d4d3345d07caef7
SnakeXrnt <67412985+SnakeXrnt@users.noreply.github.com>
2023-10-05 10:20:49 +0700

    start to take shape

--- script.js
+++ script.js
@@ -5,15 +5,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Add an event listener to the checkbox
     switchCheckbox.addEventListener("change", function() {
-    if (switchCheckbox.checked) {
-        // Checkbox is checked (switch is ON)
-        switchStatus.textContent = "Search by keyword";
-        // Add your code to handle the switch being ON
-    } else {
-        // Checkbox is not checked (switch is OFF)
-        switchStatus.textContent = "Search by region";
-        // Add your code to handle the switch being OFF
+        if (switchCheckbox.checked) {
+            // Checkbox is checked (switch is ON)
+            switchStatus.textContent = "Search by keyword";
+            window.location.href = "region.html"; // Redirect to search.html
+        } else {
+            // Checkbox is not checked (switch is OFF)
+            switchStatus.textContent = "Search by region";
+            window.location.href = "search.html"; // Redirect to region.html
+        }
+    });
+
+    const videoFeed = document.querySelector('.video-feed');
+    const videos = videoFeed.querySelectorAll('video');
+
+    videoFeed.addEventListener('scroll', function() {
+    const currentVideo = this.querySelector('video:playing');
+    const nextVideo = this.querySelector('video:not(:playing)');
+
+    const currentVideoTop = currentVideo.getBoundingClientRect().top;
+    const nextVideoTop = nextVideo.getBoundingClientRect().top;
+
+    if (currentVideoTop + currentVideo.clientHeight <= nextVideoTop) {
+        currentVideo.pause();
+        nextVideo.play();
     }
     });
+
+
 
 });
