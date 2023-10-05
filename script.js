6a3ff948859e92923a4b5a978e131428b4dbbfc4
SnakeXrnt <67412985+SnakeXrnt@users.noreply.github.com>
2023-10-05 20:34:19 +0700

    add prev next function

--- script.js
+++ script.js
@@ -1,4 +1,56 @@
 document.addEventListener('DOMContentLoaded', () => {
+    
+    // JavaScript code to handle video and description updates
+    const videos = [
+        {
+            src: "assets/video/jakarta.mp4",
+            title: "Wayne Henner Vid1",
+            description: "Description for Vid1"
+        },
+        {
+            src: "assets/video/bali.mp4",
+            title: "Wayne Henner Vid2",
+            description: "Description for Vid2"
+        },
+        {
+            src: "assets/video/jogja.mp4",
+            title: "Wayne Henner Vid3",
+            description: "Description for Vid3"
+        },
+        // Add more video objects as needed
+    ];
+
+    let currentVideoIndex = 0;
+
+    const backgroundVideo = document.getElementById("background-video");
+    const videoTitle = document.getElementById("video-title");
+    const videoDescription = document.getElementById("video-description");
+
+    const prevVideoButton = document.getElementById("prev-video");
+    const nextVideoButton = document.getElementById("next-video");
+
+    prevVideoButton.addEventListener("click", () => {
+        currentVideoIndex = (currentVideoIndex - 1 + videos.length) % videos.length;
+        updateVideo();
+        console.log("prev detected")
+    });
+
+    nextVideoButton.addEventListener("click", () => {
+        currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+        updateVideo();
+        console.log("next detected")
+    });
+
+    function updateVideo() {
+        const currentVideo = videos[currentVideoIndex];
+        backgroundVideo.src = currentVideo.src;
+        videoTitle.textContent = currentVideo.title;
+        videoDescription.textContent = currentVideo.description;
+    }
+
+    // Initialize with the first video
+    updateVideo();
+
     // Get references to the checkbox element and the status display element
     const switchCheckbox = document.getElementById("mySwitch");
     const switchStatus = document.getElementById("switchStatus");
@@ -16,23 +68,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    const videoFeed = document.querySelector('.video-feed');
-    const videos = videoFeed.querySelectorAll('video');
 
-    videoFeed.addEventListener('scroll', function() {
-    const currentVideo = this.querySelector('video:playing');
-    const nextVideo = this.querySelector('video:not(:playing)');
 
-    const currentVideoTop = currentVideo.getBoundingClientRect().top;
-    const nextVideoTop = nextVideo.getBoundingClientRect().top;
-
-    if (currentVideoTop + currentVideo.clientHeight <= nextVideoTop) {
-        currentVideo.pause();
-        nextVideo.play();
-    }
-    });
-
-    
+        
 
 
 
